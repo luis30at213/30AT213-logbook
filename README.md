61d1b4483c2f903c9d223660795d7c50d72a5ea8
dgriffoi <dgriffoi@knowmadmood.com>
2025-08-26 10:41:47 +0200

    process logs

---
+++ README.md
@@ -0,0 +1,1 @@
+# 30AT213 Luis Enrique
---
+++ process_logs.py
@@ -0,0 +1,33 @@
+import csv
+
+# Input and output file paths
+input_file = r".\my_logs.csv"
+template_file = r".\templates\logbook.html"
+output_file = r".\logbook.html"
+
+# Read the CSV file and transform the data
+with open(input_file, mode='r', encoding='utf-8') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    rows = []
+    for row in csv_reader:
+        rows.append(f"""
+        <tr>
+          <td>{row['DX']}</td>
+          <td>{row['DATE']}</td>
+          <td>{row['UTC']}</td>
+          <td>{row['FREQUENCY']}</td>
+          <td>{row['MODE']}</td>
+          <td>{row['RST'].replace('/', '')}</td>
+        </tr>
+        """)
+
+# Read the template_logbook.html file
+with open(template_file, mode='r', encoding='utf-8') as html_file:
+    template_content = html_file.read()
+
+# Replace {LOGBOOK} with the transformed rows
+updated_content = template_content.replace("{LOGBOOK}", "\n".join(rows))
+
+# Write the updated content to logbook.html
+with open(output_file, mode='w', encoding='utf-8') as html_file:
+    html_file.write(updated_content)
